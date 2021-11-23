5884c0ebbb6e30ad6b7224bfae29033945b8d0b9
tkyser-rodeo <78568187+tkyser-rodeo@users.noreply.github.com>
2021-11-23 15:21:47 -0600

    reduced compile target to just MSI

--- workspaces/desktop-release-action/src/windows.ts
+++ workspaces/desktop-release-action/src/windows.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 import { runElectronBuilder } from './shell';
 
 export const packOnWindows = async (): Promise<void> => {
-  await runElectronBuilder(`--ia32 --x64 --win nsis msi`, {
+  await runElectronBuilder(`--ia32 --x64 --win msi`, {
     CSC_LINK: core.getInput('win_csc_link'),
     CSC_KEY_PASSWORD: core.getInput('win_csc_key_password'),
   });
